c317059b120d0ff40ae6d6a1a4000f94dfa362d2
hlibine <hugo.libine@gmail.com>
2024-01-16 16:07:16 +0100

    its (not much of) a start

--- pipex/srcs/pipex.c
+++ pipex/srcs/pipex.c
@@ -1,14 +1,25 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   pipex.h                                            :+:      :+:    :+:   */
+/*   pipex.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: hlibine <hlibine@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/01/14 23:52:21 by hlibine           #+#    #+#             */
-/*   Updated: 2024/01/16 15:18:29 by hlibine          ###   ########.fr       */
+/*   Created: 2024/01/14 23:52:06 by hlibine           #+#    #+#             */
+/*   Updated: 2024/01/16 15:24:39 by hlibine          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-# include <fcntl.h>
-# include <stdio.h>
+#include "../pipex.h"
+
+int	main(int argc, char **argv)
+{
+	int	*infile;
+	int	*outfile;
+
+	if (argc != 5)
+		exit(1);
+	infile = open(argv[1], O_RDONLY);
+	outfile = open(argv[4], O_WRONLY);
+	
+}
