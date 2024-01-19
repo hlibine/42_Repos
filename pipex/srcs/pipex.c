5a1f518b0545bac35b4178b99cb5e159e08c6f25
hlibine <hugo.libine@gmail.com>
2024-01-19 16:21:03 +0100

    yeah i still dont get it

--- pipex/srcs/pipex.c
+++ pipex/srcs/pipex.c
@@ -6,59 +6,58 @@
 /*   By: hlibine <hlibine@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/14 23:52:06 by hlibine           #+#    #+#             */
-/*   Updated: 2024/01/18 15:44:18 by hlibine          ###   ########.fr       */
+/*   Updated: 2024/01/19 16:20:00 by hlibine          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../pipex.h"
 
-void	parent_ps(int f2, char* cmd)
+void	excec(const  char *cmd, char **envp)
 {
-	int	status;
+	char **s_cmd;
+	char *path;
 
-	waitpid(-1, &status, 0);
-	dup2(f2, ...);
-	dup2(end[0], ...);
-	close[1];
-	close(f2)
-	execv();
-	exit(EXIT_FAILURE)
+	s_cmd = ft_split(cmd, ' ');
+	path = getpath(s_cmd[0], envp);
+	exit(EXIT_SUCCESS);
 }
 
-void	child_ps(int f1, char* cmd)
+void	child_ps(int e_fd, char **argv, char **envp)
 {
-	dup2(f1, STDIN_FILENO);
-	dup2(end[1], STDOUT_FILENO);
-	close(end[0]);
-	close(f1);
-	execve();
-	exit(EXIT_FAILURE);
+	int	fd;
+
+	fd = open(av[1], 0);
+	dup2(fd, STDIN_FILENO);
+	dup2(e_fd[1], STDOUT_FILENO);
+	close(e_fd[0]);
+	exex(argv[2], envp);
 }
 
-void	pipex(int f1, int f2, char **argv, char** envp)
+void	parent_ps(int e_fd, char **argv, char **envp)
 {
-	int		end[2];
-	pid_t	parent;
+	int	fd;
 
-	pipe(end);
-	parent = fork();
-	if (parent < 0)
-		px_error("fork");
-	if (!parent)
-		child_ps(f1, cmd1);
-	else
-		parent_ps(f2, cmd2);
+	fd = open(argv[4], 1);
+	dup2(fd, STDOUT_FILENO);
+	dup2(e_fd[0], STDIN_FILENO);
+	close(e_fd[1]);
+	excec(argv[3], envp);
 }
 
 int	main(int argc, char **argv, char **envp)
 {
-	int	*infile;
-	int	*outfile;
+	int		fd[2]
+	pid_t	pid;
 
 	if (argc != 5)
 		px_error("not enough / too many arguments");
-	infile = open(argv[1], O_RDONLY);
-	outfile = open(argv[4], O_WRONLY);
-	pipex(infile, outfile, argv, envp);
+	if (pipe(fd) == -1)
+		px_error("pipe error");
+	pid = fork();
+	if (pid == -1)
+		exit(-1);
+	if (!pid)
+		child_ps(fd, argv, envp);
+	parent_ps(fd, argv, envp);
 	return (0);
 }
